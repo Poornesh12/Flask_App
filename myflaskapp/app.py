17d1eaf18572a026dcff1b19702eccac44d13639
Poornesh15 <poornesh.p@sigmoidanalytics.com>
2024-06-27 18:22:40 +0530

    edited back

--- myflaskapp/app.py
+++ myflaskapp/app.py
@@ -4,9 +4,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hell')
+@app.route('/')
 def hello_world():
-    return 'Hello, World'
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(debug=True)
